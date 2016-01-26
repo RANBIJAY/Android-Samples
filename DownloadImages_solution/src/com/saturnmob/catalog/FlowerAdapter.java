2f39c94bc36f61d5b8a90ce306198addc66b79f5
RAN <kumar.ranbijay@gmail.com>
2016-01-26 23:39:25 +0530

    Working with Binary Resources - Downloading images from a web server.

---
+++ DownloadImages_solution/src/com/saturnmob/catalog/FlowerAdapter.java
@@ -0,0 +1,47 @@
+package com.saturnmob.catalog;
+
+import java.util.List;
+
+import android.app.Activity;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.saturnmob.catalog.R;
+import com.saturnmob.catalog.model.Flower;
+
+public class FlowerAdapter extends ArrayAdapter<Flower> {
+
+	private Context context;
+	private List<Flower> flowerList;
+
+	public FlowerAdapter(Context context, int resource, List<Flower> objects) {
+		super(context, resource, objects);
+		this.context = context;
+		this.flowerList = objects;
+	}
+
+	@Override
+	public View getView(int position, View convertView, ViewGroup parent) {
+
+		LayoutInflater inflater = 
+				(LayoutInflater) context.getSystemService(Activity.LAYOUT_INFLATER_SERVICE);
+		View view = inflater.inflate(R.layout.item_flower, parent, false);
+
+		//Display flower name in the TextView widget
+		Flower flower = flowerList.get(position);
+		TextView tv = (TextView) view.findViewById(R.id.textView1);
+		tv.setText(flower.getName());
+		
+		//Display flower photo in ImageView widget
+		ImageView image = (ImageView) view.findViewById(R.id.imageView1);
+		image.setImageBitmap(flower.getBitmap());
+		
+		return view;
+	}
+
+}
