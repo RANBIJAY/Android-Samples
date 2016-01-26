c37e044c4acfdfffee84c2e71ef798808ab08749
RAN <kumar.ranbijay@gmail.com>
2016-01-26 23:30:56 +0530

    Working with Binary Resources - Displaying web service data in a ListActivity.

---
+++ DisplayList_solution/src/com/saturnmob/catalog/MainActivity.java
@@ -0,0 +1,118 @@
+package com.saturnmob.catalog;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import android.app.ListActivity;
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.saturnmob.catalog.R;
+import com.saturnmob.catalog.model.Flower;
+import com.saturnmob.catalog.parsers.FlowerJSONParser;
+
+public class MainActivity extends ListActivity {
+
+	TextView output;
+	ProgressBar pb;
+	List<MyTask> tasks;
+	
+	List<Flower> flowerList;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		
+		pb = (ProgressBar) findViewById(R.id.progressBar1);
+		pb.setVisibility(View.INVISIBLE);
+		
+		tasks = new ArrayList<>();
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		if (item.getItemId() == R.id.action_get_data) {
+			if (isOnline()) {
+				requestData("http://services.hanselandpetal.com/secure/flowers.json");
+			} else {
+				Toast.makeText(this, "Network isn't available", Toast.LENGTH_LONG).show();
+			}
+		}
+		return false;
+	}
+
+	private void requestData(String uri) {
+		MyTask task = new MyTask();
+		task.execute(uri);
+	}
+
+	protected void updateDisplay() {
+		//Use FlowerAdapter to display data
+		FlowerAdapter adapter = new FlowerAdapter(this, R.layout.item_flower, flowerList);
+		setListAdapter(adapter);
+	}
+	
+	protected boolean isOnline() {
+		ConnectivityManager cm = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+		NetworkInfo netInfo = cm.getActiveNetworkInfo();
+		if (netInfo != null && netInfo.isConnectedOrConnecting()) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+	
+	private class MyTask extends AsyncTask<String, String, String> {
+
+		@Override
+		protected void onPreExecute() {
+			if (tasks.size() == 0) {
+				pb.setVisibility(View.VISIBLE);
+			}
+			tasks.add(this);
+		}
+		
+		@Override
+		protected String doInBackground(String... params) {
+			
+			String content = HttpManager.getData(params[0], "feeduser", "feedpassword");
+			return content;
+		}
+		
+		@Override
+		protected void onPostExecute(String result) {
+			
+			tasks.remove(this);
+			if (tasks.size() == 0) {
+				pb.setVisibility(View.INVISIBLE);
+			}
+			
+			if (result == null) {
+				Toast.makeText(MainActivity.this, "Web service not available", Toast.LENGTH_LONG).show();
+				return;
+			}
+			
+			flowerList = FlowerJSONParser.parseFeed(result);
+			updateDisplay();
+
+		}
+		
+	}
+
+}
